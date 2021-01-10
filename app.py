8328e922811786516896694f1c3ded3fef3ea5f0
pcpkomputer <terrificsubs@gmail.com>
2021-01-10 07:17:35 +0700

    ...

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template
+import json
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="koreanmovieclassification"
+)
 
 app = Flask(__name__)
 
@@ -12,7 +21,53 @@ def dashboard():
 
 @app.route("/dataset")
 def dataset():
-    return render_template("dataset.html");
+
+    mydb.connect()
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM dataset")
+    data = cursor.fetchall()
+
+    # {
+    #       no:1,
+    #       judul:"sadasd",
+    #       stasiuntv:"MBS",
+    #       genre:"MM",
+    #       writer:"asdasd",
+    #       director:"asdasddd",
+    #       actor:"meme",
+    #       status:"A"
+    # }
+
+    payload = []
+    for index, x in enumerate(data):
+        obj = {}
+        obj["no"]=index+1
+        obj["judul"]=x[0]
+        obj["stasiuntv"]=x[1]
+        obj["genre"]=x[2]
+        obj["writer"]=x[3]
+        obj["director"]=x[4]
+        obj["actor"]=x[5]
+        obj["status"]=x[10]
+        payload.append(obj)
+
+    return render_template("dataset.html", dataset=json.dumps(payload));
+
+@app.route("/preprocessing")
+def preprocessing():
+    return render_template("preprocessing.html");
+
+@app.route("/klasifikasi")
+def klasifikasi():
+    return render_template("klasifikasi.html");
+
+@app.route("/pengujian")
+def pengujian():
+    return render_template("pengujian.html");
+
+@app.route("/keluar")
+def keluar():
+    return "keluar";
 
 if __name__=='__main__':
     app.run(debug=True)
